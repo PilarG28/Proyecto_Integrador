698d43c1603ecb010715e5c8a8a8640cadb05fc9
PilarG28 <mariadelpilarguglielmetti@hotmail.com>
2022-08-22 16:46:12 -0300

    Back

---
+++ Proyecto_Integrador/BackEnd/src/main/java/com/portfolio/pilarg/Interface/IPersonaService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.pilarg.Interface;
+
+import com.portfolio.pilarg.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    public void savePersona(Persona persona);
+    public void deletePersona(Long id);
+    public Persona findPersona(Long id);
+}
---
+++ Proyecto_Integrador/BackEnd/src/main/java/com/portfolio/pilarg/PilargApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.pilarg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilargApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilargApplication.class, args);
+	}
+
+}
